ff8bc006a2fdd30751666030869b9ffd5d1e4f7b
Mohammed Berila <mberila@e1r6p15.1337.ma>
2025-04-04 14:32:23 +0000

    d

---
+++ PHILO/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 17:14:07 by mberila           #+#    #+#             */
+/*   Updated: 2025/03/17 17:19:37 by mberila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+#endif
