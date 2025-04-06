c555d9b735c532c2cd4ce83496a16809dd348f1a
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-04-06 11:01:46 +0100

    New Commit

--- PHILO/philo.c
+++ PHILO/philo.c
@@ -6,13 +6,27 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 13:47:22 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/04 14:59:43 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/06 10:32:05 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
+void	clean_exit(char *str)
+{
+	printf(RED"%s"RESET, str);
+	exit(EXIT_FAILURE);
+}
+
 int main(int ac, char *av[])
 {
+	t_table *table;
+
+	table = NULL;
+	(void)av;
+	if (ac != 5 && ac != 6)
+		clean_exit("Invalid number arguments\n");
+	if (!parse_arguments(ac, av, table))
+		clean_exit("Ivalid arguments");
 	return (0);
 }
--- PHILO/philo.h
+++ PHILO/philo.h
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:14:07 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/04 15:50:05 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/06 10:24:46 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@
 #include <sys/time.h>
 #include <limits.h>
 
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m"
+
 /*
 - TABLE 
 - ARGS: ./philo 5 800 200 200 [5]
@@ -28,11 +36,13 @@
 
 typedef struct s_table
 {
-	long	philo_nbr;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	meals_limit;
+	int	philo_nbr;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_times_to_eat;
 }	t_table;
+
+int	parse_arguments(int ac, char *av[], t_table *table);
 
 #endif
